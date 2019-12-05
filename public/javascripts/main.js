ad741759a1f49ffa08cccd147f93375dd6872341
Harry Zumwalt <harry.zum@gmail.com>
2019-12-04 22:37:26 -0600

    allow cors

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
   function getChallengerTier() {
     const apiKey = "RGAPI-d895e4c6-3835-4a3f-8cf7-afa5de6299b0";
-    const url = "https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5";
+    const url = "https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5";
     const config = {
       headers: {'Access-Control-Allow-Origin': '*'}
     };
